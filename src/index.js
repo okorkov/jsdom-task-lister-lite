a8288beacfb7db086f7781a821e62e208b7f20f9
okorkov <alex.okorkov@gmail.com>
2021-02-10 15:02:37 -0800

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+    let value = document.getElementById('new-task-description').value
+    let liTag = document.createElement('li')
+    let button = document.createElement('button')
+    liTag.innerHTML = value
+    button.innerHTML = 'X'
+    let tasks = document.getElementById("tasks")
+    tasks.appendChild(liTag)
+    liTag.appendChild(button)
+    event.preventDefault();
+    document.getElementById('new-task-description').value = ''
+    button.addEventListener("click", function(event) {
+      liTag.remove()
+      event.preventDefault();
+    })
+  })
 });
+
